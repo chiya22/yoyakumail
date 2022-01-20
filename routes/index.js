6b2cf8bb57349eebea2c91048dce788f9248851b
10mo8 <10mo8since2020@gmail.com>
2022-01-20 13:57:22 +0900

    リファクタリング

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,12 @@ const router = express.Router();
 const m_searchinfos = require("../model/searchinfos");
 const m_yoyakus = require("../model/yoyakus");
 
-const yoyakuweb = require("../util/yoyakuweb");
-const common = require("../util/common");
+const common = require("../util/common")
+const yoyakuinfo = require("../util/yoyakuinfo");
 
 const log4js = require("log4js");
+const yoyakus = require("../model/yoyakus");
 const logger = log4js.configure("./config/log4js-config.json").getLogger();
-
-const fs = require("fs");
-const iconv = require("iconv-lite");
-const readline = require("readline");
 
 // 検索条件の一覧を表示する
 router.get("/", function (req, res) {
@@ -45,8 +42,8 @@ router.get("/yoyakudelete/:id", function (req, res) {
   (async () => {
 
     // idより予約一覧を取得し、返却する
-    await m_searchinfos.remove(req.params.id); // 検索条件
     await m_yoyakus.removeByIdSearch(req.params.id); // 予約情報
+    await m_searchinfos.remove(req.params.id); // 検索条件
 
     // 検索条件情報の一覧を取得する
     await m_searchinfos.find();
@@ -65,93 +62,13 @@ router.post("/yoyakus", function (req, res) {
     const yyyymmdd_riyou_start = req.body.yyyymmdd_riyou_start;
     const yyyymmdd_riyou_end = req.body.yyyymmdd_riyou_end;
 
-    // 予約システムより指定した期間の予約情報をダウンロード
-    await yoyakuweb.dlyoyakuinfo(yyyymmdd_addupd_start, yyyymmdd_addupd_end, yyyymmdd_riyou_start, yyyymmdd_riyou_end);
-
-    // ダウンロードディレクトリにあるファイル群を読み込む
-    let targetfilename = "";
-
     // ID用プレフィックス
-    let yyyymmddhhmmss_proc = common.getTodayTime();
+    const yyyymmddhhmmss_proc = common.getTodayTime();
 
-    await fs.readdirSync(process.env.YOYAKU_DL_PATH).forEach((filename) => {
-      
-      // 予約情報ダウンロードファイルの場合
-      if (filename.slice(0, 11) === "kakuninsho_") {
-        targetfilename = filename;
+    // 予約システムより指定した期間の予約情報をダウンロード
+    await yoyakuinfo.dlyoyakuinfo(yyyymmdd_addupd_start, yyyymmdd_addupd_end, yyyymmdd_riyou_start, yyyymmdd_riyou_end);
 
-        // id設定用プレフィックス
-        let max_id_yoyaku = 1;
-
-        // csvファイルはShift-JISのため
-        const src = fs.createReadStream(process.env.YOYAKU_DL_PATH + "\\" + filename).pipe(iconv.decodeStream("Shift_JIS"));
-
-        // 1行ごとに読み込む
-        const rl = readline.createInterface({
-          input: src,
-          output: process.stdout,
-          terminal: false,
-        });
-
-        // 1行ごとの処理
-        rl.on("line", (chunk) => {
-          const linecontents = chunk.split(",");
-
-          // ヘッダーは飛ばす
-          if (linecontents[0] !== "管理ID" && linecontents[0] !== "") {
-            // 予約情報用オブジェクト
-            let inObj = {};
-            inObj.id = "Y" + yyyymmddhhmmss_proc + ("00000" + max_id_yoyaku).slice(-5);
-            inObj.id_search = "Y" + yyyymmddhhmmss_proc;
-            inObj.id_kanri = linecontents[0];
-            inObj.nm_room = linecontents[1];
-            inObj.yyyymmdd_yoyaku = linecontents[2].replace(/\//g, "");
-            inObj.time_start = linecontents[3];
-            inObj.time_end = linecontents[4];
-            inObj.price = linecontents[5] ? linecontents[5] : 0;
-            inObj.ymd_uketuke = linecontents[6].replace(/\//g, "");
-            inObj.status_shiharai = linecontents[7];
-            inObj.nm_nyuryoku = linecontents[8];
-            inObj.nm_riyousha = linecontents[9];
-            inObj.nm_room_seishiki = linecontents[10];
-            inObj.type_room = linecontents[11];
-            inObj.no_keiyakusha = linecontents[12];
-            inObj.nm_keiyakusha = linecontents[13];
-            inObj.nm_tantousha = linecontents[14];
-            inObj.telno = linecontents[15];
-            inObj.faxno = linecontents[16];
-            inObj.email = linecontents[17];
-            inObj.kubun = linecontents[18];
-            inObj.address = linecontents[19];
-            inObj.quantity = linecontents[20] ? linecontents[20] : 0;
-            inObj.tanka = linecontents[21] ? linecontents[21] : 0;
-            inObj.caution = linecontents[22];
-            inObj.memo = linecontents[23];
-            inObj.yyyymmddhhmmss_created = yyyymmddhhmmss_proc;
-
-            (async () => {
-              await m_yoyakus.insert(inObj);
-              logger.info(`予約情報ID：${inObj.id}`);
-            })();
-            // id設定用プレフィックスに+1
-            max_id_yoyaku += 1;
-          }
-        });
-
-        // 終了時には処理した対象ファイルをリネームする
-        src.on("end", () => {
-          fs.rename(process.env.YOYAKU_DL_PATH + "\\" + targetfilename, process.env.YOYAKU_DL_PATH + "\\old_" + targetfilename, (err) => {
-            if (err) {
-              logger.info(`${targetfilename}ファイルは存在しません：${new Date()}`);
-              throw err;
-            }
-          });
-        });
-      }
-    });
-
-    // 検索条件を登録する
-    // 検索条件用オブジェクト
+    // 検索条件情報を登録する
     let inObjSearch = {};
     inObjSearch.id = "Y" + yyyymmddhhmmss_proc;
     inObjSearch.yyyymmdd_addupd_start = yyyymmdd_addupd_start;
@@ -160,17 +77,14 @@ router.post("/yoyakus", function (req, res) {
     inObjSearch.yyyymmdd_riyou_end = yyyymmdd_riyou_end;
     inObjSearch.status = "1";
     inObjSearch.yyyymmddhhmmss_created = yyyymmddhhmmss_proc;
-
     await m_searchinfos.insert(inObjSearch);
     logger.info(`検索条件情報ID：${inObjSearch.id}`);
 
-    const retObjSearchinfo = await m_searchinfos.findPKey("Y" + yyyymmddhhmmss_proc);
-    const retObjYoyakus = await m_yoyakus.findByIdSearch("Y" + yyyymmddhhmmss_proc);
+    // ダウンロードしたファイルより予約情報をテーブルへ登録する
+    let yoyakulist = [];
+    yoyakulist = await yoyakuinfo.filetodb(yyyymmddhhmmss_proc);
 
-    res.render("yoyakus", {
-      searchinfo: retObjSearchinfo[0],
-      yoyakus: retObjYoyakus,
-    });
+    res.redirect("/");
   })();
 });
 
---
+++ util/yoyakuinfo.js
@@ -0,0 +1,190 @@
+const log4js = require("log4js");
+const logger = log4js.configure("./config/log4js-config.json").getLogger();
+const puppeteer = require("puppeteer");
+
+const fs = require("fs");
+const iconv = require("iconv-lite");
+const readline = require("readline");
+
+const m_yoyakus = require("../model/yoyakus");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+//　予約情報ダウンロード
+const dlyoyakuinfo = async (yyyymmdd_addupd_start, yyyymmdd_addupd_end, yyyymmdd_riyou_start, yyyymmdd_riyou_end) => {
+  const browser = await puppeteer.launch({ headless: false });
+
+  let page = await browser.newPage();
+
+  await page.goto(process.env.YOYAKU_URL, { waitUntil: "domcontentloaded" });
+
+  // ログイン
+  await page.type('input[name="in_office"]', process.env.YOYAKU_LOGIN_ID);
+  await page.type('input[name="in_opassword"]', process.env.YOYAKU_LOGIN_PASSWORD);
+  await page.click("body > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > form > table:nth-child(2) > tbody > tr > td:nth-child(2) > input");
+
+  await page.waitForTimeout(1000);
+  // await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+
+  // 「予約検索」をクリック
+  const menu = await page.$("body > table > tbody > tr > td > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(8) > td:nth-child(2) > input[type=image]:nth-child(9)");
+  await menu.click();
+
+  await page.waitForTimeout(1000);
+
+  // 「新規予約確認」をクリック
+  const shinki = await page.$("body > div:nth-child(4) > table:nth-child(3) > tbody > tr > th:nth-child(3) > a");
+  await shinki.click();
+
+  await page.waitForTimeout(1000);
+
+  // 登録日/更新日の設定
+  const yyyymm_addupd_start = yyyymmdd_addupd_start.slice(0, 6);
+  const dd_addupd_start = "" + Number(yyyymmdd_addupd_start.slice(-2));
+  const yyyymm_addupd_end = yyyymmdd_addupd_end.slice(0, 6);
+  const dd_addupd_end = "" + Number(yyyymmdd_addupd_end.slice(-2));
+  const yyyymm_riyou_start = yyyymmdd_riyou_start.slice(0, 6);
+  const dd_riyou_start = "" + Number(yyyymmdd_riyou_start.slice(-2));
+  const yyyymm_riyou_end = yyyymmdd_riyou_end.slice(0, 6);
+  const dd_riyou_end = "" + Number(yyyymmdd_riyou_end.slice(-2));
+
+  await page.select('select[name="s_tourokuYm"]', yyyymm_addupd_start);
+  await page.select('select[name="s_tourokudd"]', dd_addupd_start);
+  await page.select('select[name="e_tourokuYm"]', yyyymm_addupd_end);
+  await page.select('select[name="e_tourokudd"]', dd_addupd_end);
+  await page.select('select[name="s_riyouYm"]', yyyymm_riyou_start);
+  await page.select('select[name="s_riyoudd"]', dd_riyou_start);
+  await page.select('select[name="e_riyouYm"]', yyyymm_riyou_end);
+  await page.select('select[name="e_riyoudd"]', dd_riyou_end);
+
+  // 「検索」ボタンをクリック
+  await page.click("body > div:nth-child(4) > form > table:nth-child(1) > tbody > tr > td:nth-child(4) > input");
+
+  await page.waitForTimeout(1000);
+
+  // Promptが出たら必ずOKとする
+  page.on("dialog", async (dialog) => {
+    await dialog.accept();
+  });
+
+  // ダウンロード先を修正
+  await page._client.send("Page.setDownloadBehavior", {
+    behavior: "allow",
+    downloadPath: "C:\\download\\customer",
+  });
+
+  // 隠し項目へ値を設定
+  // proc_flg
+  await page.$eval('input[name="proc_flg"]', (el) => (el.value = "download"));
+  // OLDShoriTabMenu
+  await page.$$eval('input[name="OLDShoriTabMenu"]', (els) => {
+    els.forEach((el) => (el.value = "2"));
+  });
+  // target,action
+  await page.$eval('form[name="formlist"]', (form) => {
+    form.action = "/studio/office/MemberEntry/OLDSTabSelect.php";
+    form.target = "";
+  });
+
+  // formをsubmit
+  await page.$eval('form[name="formlist"]', (form) => form.submit());
+
+  await logger.info(`予約情報をダウンロードしました`);
+  await page.waitForTimeout(1000);
+  await browser.close();
+
+  logger.info(`予約情報ダウンロード終了`);
+};
+
+// ダウンロードディレクトリにあるファイル群を読み込む
+const filetodb = (yyyymmddhhmmss_proc) => {
+
+  let targetfilename = "";
+
+  fs.readdirSync(process.env.YOYAKU_DL_PATH).forEach((filename) => {
+
+    // 予約情報ダウンロードファイルの場合
+    if (filename.slice(0, 11) === "kakuninsho_") {
+
+      targetfilename = filename;
+
+      // id設定用プレフィックス
+      let max_id_yoyaku = 1;
+
+      // csvファイルはShift-JISのため
+      const src = fs.createReadStream(process.env.YOYAKU_DL_PATH + "\\" + filename).pipe(iconv.decodeStream("Shift_JIS"));
+
+      // 1行ごとに読み込む
+      const rl = readline.createInterface({
+        input: src,
+        output: process.stdout,
+        terminal: false,
+      });
+
+      // 1行ごとの処理
+      rl.on("line", (chunk) => {
+
+        const linecontents = chunk.split(",");
+
+        // ヘッダーは飛ばす
+        if (linecontents[0] !== "管理ID" && linecontents[0] !== "") {
+          // 予約情報用オブジェクト
+          let inObj = {};
+          inObj.id = "Y" + yyyymmddhhmmss_proc + ("00000" + max_id_yoyaku).slice(-5);
+          inObj.id_search = "Y" + yyyymmddhhmmss_proc;
+          inObj.id_kanri = linecontents[0];
+          inObj.nm_room = linecontents[1];
+          inObj.yyyymmdd_yoyaku = linecontents[2].replace(/\//g, "");
+          inObj.time_start = linecontents[3];
+          inObj.time_end = linecontents[4];
+          inObj.price = linecontents[5] ? linecontents[5] : 0;
+          inObj.ymd_uketuke = linecontents[6].replace(/\//g, "");
+          inObj.status_shiharai = linecontents[7];
+          inObj.nm_nyuryoku = linecontents[8];
+          inObj.nm_riyousha = linecontents[9];
+          inObj.nm_room_seishiki = linecontents[10];
+          inObj.type_room = linecontents[11];
+          inObj.no_keiyakusha = linecontents[12];
+          inObj.nm_keiyakusha = linecontents[13];
+          inObj.nm_tantousha = linecontents[14];
+          inObj.telno = linecontents[15];
+          inObj.faxno = linecontents[16];
+          inObj.email = linecontents[17];
+          inObj.kubun = linecontents[18];
+          inObj.address = linecontents[19];
+          inObj.quantity = linecontents[20] ? linecontents[20] : 0;
+          inObj.tanka = linecontents[21] ? linecontents[21] : 0;
+          inObj.caution = linecontents[22];
+          inObj.memo = linecontents[23];
+          inObj.yyyymmddhhmmss_created = yyyymmddhhmmss_proc;
+
+          (async () => {
+            await m_yoyakus.insert(inObj);
+          })();
+          logger.info(`予約情報ID：${inObj.id}`);
+
+          // id設定用プレフィックスに+1
+          max_id_yoyaku += 1;
+
+        }
+      });
+
+      // 終了時には処理した対象ファイルをリネームする
+      src.on("end", () => {
+        fs.rename(process.env.YOYAKU_DL_PATH + "\\" + targetfilename, process.env.YOYAKU_DL_PATH + "\\old_" + targetfilename, (err) => {
+          if (err) {
+            logger.info(`${targetfilename}ファイルは存在しません：${new Date()}`);
+            throw err;
+          }
+        });
+      });
+    }
+  })
+};
+
+module.exports = {
+  dlyoyakuinfo,
+  filetodb,
+};
